8ad42e85aa50b544911f450069b73bd2b896187c
alona007 <alona.olyksashenko@gmail.com>
2024-02-24 23:22:46 +0100

    5

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+    const totalWidth = contentWidth + paddingWidth + paddingWidth + borderWidth + borderWidth;
     return totalWidth;
 }
 
